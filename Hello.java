628ddde84c82234ccf722dc100c66e1a8271f828
meltingmelvin <75199617+meltingmelvin@users.noreply.github.com>
2020-12-16 02:54:58 +0900

    Delete Hello.java

--- Hello.java
+++
@@ -1,8 +0,0 @@
-// Hello.java
-
-public class Hello {
-   public static void main(String[] args) {
-	System.out.println("Hello, world from GIT!");
-	System.out.println("Changes after First commit!");
-	}
-}
